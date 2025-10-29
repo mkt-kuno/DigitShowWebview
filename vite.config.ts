c52eceeba64e9a8f53a30bc198d82ae3b045ebee
Makoto KUNO <mkt-kuno@iis.u-tokyo.ac.jp>
2025-10-29 14:23:11 +0900

    SVG更新、ビルド設定を調整

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: true,
+    sourcemap: false,
+    target: 'es2020'
+  },
   server: {
     proxy: {
       '/v1': {
